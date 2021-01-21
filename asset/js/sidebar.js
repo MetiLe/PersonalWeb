8e31400a2baadf12c090cf8c041c98f1670ecba6
Meti Leggese <se.meti.leggese@gmail.com>
2021-01-21 10:52:25 +0300

    overview is changed to index and contact page is changed to blog

--- asset/js/sidebar.js
+++ asset/js/sidebar.js
@@ -1,8 +1,3 @@
-/*!
- * Sidebar for Bootstrap 4
- * Copyright Zdeněk Papučík
- * MIT License
- */
 (function($) {
 
   // toggle sidebar
@@ -11,3 +6,35 @@
   });
 
 }(jQuery));
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          $('.filter').show('1000');
+      }
+      else
+      {
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+
+          if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+      }
+        $(this).addClass("active");
+      });
+});
+/*	end gallery */
+
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+      openEffect: "none",
+      closeEffect: "none"
+  });
+});
+ 
+
